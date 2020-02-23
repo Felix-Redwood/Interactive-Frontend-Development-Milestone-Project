b3d62568a657de5d9313e0685b5a9affda41191f
Ubuntu <ubuntu@ip-172-31-81-1.ec2.internal>
2020-02-23 16:12:08 +0000

    updated email.js. It is now working!

--- assets/js/email.js
+++ assets/js/email.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
                                 <form action="" method="post">
                                     <!-- this form will gather the emails -->
                                     <label for="user-email" class="">Email:</label> <br>
-                                    <input type="text" name="user-email" id="uemail" value=" Example@email.com " class="email-input" />
+                                    <input type="text" name="uemail" id="uemail" value=" Example@email.com " class="email-input" />
                                     <small class="form-text">We'll never share your email with anyone else.</small>
                                     <button type="button" class="btn btn-warning" onclick="collectEmail()">Submit:</button>
                                 </form>
@@ -21,10 +21,10 @@ $(document).ready(function() {
     // this resets the emailDesc and emailSubmitContainer HTML every time the page is refreshed
 });
 
+var emailList = [];
 function collectEmail(event) {
     var uemail = $("#uemail").val();
-    var emailList = [];
-    
+    console.log(uemail);
     emailList.push(uemail);
     console.log(emailList);
-}
+};
